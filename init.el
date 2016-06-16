958ea2d65f278e8882e719ac38ddfa16466592b5
peoplewhite <shonshon7@gmail.com>
2016-06-16 14:21:56 +0800

    [add] add init.el file

---
+++ init.el
@@ -0,0 +1,9 @@
+;;; init.el --- Where all the magic begins
+;;
+;;
+;; Set init file for custom settings
+;;
+;; Added by Package.el.  This must come before configurations of
+;; installed packages.  Don't delete this line.  If you don't want it,
+;; just comment it out by adding a semicolon to the start of the line.
+;; You may delete these explanatory comments.
